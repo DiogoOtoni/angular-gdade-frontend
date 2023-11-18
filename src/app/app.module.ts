00bf3b050c2f2aceae68a8b52fbedc0ac88a1705
Diogo Otoni <diogootoni@hotmail.com>
2023-11-17 23:00:54 -0300

    Feat: Refatoracao da busca - otimizada e pronta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 import{ FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-
 import { HomeComponent } from './pages/home/home.component';
-
-import { JobsDetailsComponent } from './pages/jobs-details/jobs-details.component';
-
 import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
 import { AplicationCardComponent } from './components/aplication-card/aplication-card.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { JobsFormComponent } from './components/jobs-form/jobs-form.component';
 
 import { AplicationsService } from './services/aplications.service';
-import { AppRoutingModule } from './app-routing.module';
 
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-import { JobsSearchComponent } from './components/jobs-search/jobs-search.component';
+
 
 @NgModule({
   declarations: [
@@ -27,9 +23,7 @@ import { JobsSearchComponent } from './components/jobs-search/jobs-search.compon
     HeaderMenuComponent,
     AplicationCardComponent,
     FooterComponent,
-    JobsFormComponent,
-    JobsDetailsComponent,
-    JobsSearchComponent
+    JobsFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, catchError, of } from 'rxjs';
 
 import { AplicationsService } from 'src/app/services/aplications.service';
@@ -9,21 +9,47 @@ import {IJob} from 'src/app/model/IJob';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-	jobs$: Observable<IJob[]> | null = null;
+	jobsData: IJob[] | null = null;
+	inputSearchValue: string = '';
+	inputSelectValue: string = '';
 
-	constructor(private aplicationsService: AplicationsService){
-		this.refresh();
-	}
+	constructor(private aplicationsService: AplicationsService){}
 
-	refresh(){
-		this.jobs$ = this.aplicationsService.list().pipe(
-			catchError(error => {
-				console.log(error);
-				return of([]);
-			})
+	ngOnInit(): void {
+		this.aplicationsService.list().subscribe(
+			data => {
+				this.jobsData = data
+			},
+			error => console.log(error)
 		)
 	}
 
+	findBySearch() {
+		const regex = new RegExp(this.inputSearchValue, "ig");
+		if (this.jobsData != null && this.jobsData.length > 0 && this.inputSelectValue != '') {
+			return this.jobsData.filter((item) => {
+				if (this.inputSelectValue == 'jobName') {
+					const myString = item.jobName;
+					return myString.match(regex);
+				};
+				if (this.inputSelectValue == 'descricao') {
+					const myString = item.descricao;
+					return myString.match(regex);
+				};
+				if (this.inputSelectValue == 'empresa') {
+					const myString = item.empresa;
+					return myString.match(regex);
+				};
+				if (this.inputSelectValue == 'empAnun') {
+					const myString = item.empAnun;
+					return myString.match(regex);
+				};
+				return false
+			});
+		}else{
+			return this.jobsData;
+		}
+	}
 }
